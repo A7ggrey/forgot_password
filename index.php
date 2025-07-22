5e229fb96def36a88dd1dadafdb40f48940bbc35
Aggrey Kiprop <aggreykiprop60@gmail.com>
2025-07-22 16:28:08 +0300

    updates

--- index.php
+++ index.php
@@ -1,24 +1,53 @@
 <?php
+
 session_start();
 
-?>
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
 
-<html lang="en">
+?>
+<!DOCTYPE html>
+<html>
 <head>
+    <title>Home</title>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
-    <form action="./change.php" method="post">
-        <p>
-            <input type="text" name="email" id="" placeholder="Email">
-        </p>
-        <p>
-            <input type="submit" value="Send Password" name="send_password">
-        </p>
-    </form>
+    
+    <h1>Home</h1>
+    
+    <?php if (isset($user)): ?>
+        
+        <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
+        
+        <p><a href="logout.php">Log out</a></p>
+        
+    <?php else: ?>
+        
+        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+        
+    <?php endif; ?>
     
 </body>
 </html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
